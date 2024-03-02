f2a9918ea1415d6539aaaaf953780f0e63c9da28
Robson Andrade <robson.andrade19@gmail.com>
2024-03-02 15:13:12 -0300

    chore: adjust Jest configuration for test coverage and verbosity

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,11 @@ const createJestConfig = nextJest({ dir: './' })
 const customJestConfig = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     testEnvironment: 'jest-environment-jsdom',
+    collectCoverageFrom: [
+        "src/**/*.{js,jsx,ts,tsx}",
+        "!src/**/*.d.ts", 
+        "!src/**/*styles.ts" 
+    ],
 }
 
 module.exports = createJestConfig(customJestConfig)
